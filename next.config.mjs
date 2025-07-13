63f25863aefc94b67c8bf42a20cb1ab65774c5bc
Lucas Helmvik <113353252+Benim22@users.noreply.github.com>
2025-07-13 12:33:40 +0200

    .

--- next.config.mjs
+++ next.config.mjs
@@ -28,7 +28,7 @@ const nextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "img-src 'self' data: blob: https: http:",
               "font-src 'self' https://fonts.gstatic.com",
-              "connect-src 'self' https: wss: ws:",
+              "connect-src 'self' https: wss: ws: http://192.168.1.103",
               "media-src 'self' https: data:",
               "object-src 'none'",
               "base-uri 'self'",
